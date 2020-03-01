020b02a09af608dff0057fcb61f66eb596dcb58a
Rafal <rostkos@gmail.com>
2020-03-01 14:27:48 +0100

    Zadanie domowe

---
+++ src/Lekarstwa.java
@@ -0,0 +1,13 @@
+public class Lekarstwa {
+
+
+    public static void main(String[] args) {
+
+
+        String[] spisLekow = new String[5]{"Paracetamol", "Ketonal", "Ibum", "Apap", "Polopiryna"}
+        double[] cena = new String[5]{10,14,12,23,43}
+
+
+    }
+    }
+
